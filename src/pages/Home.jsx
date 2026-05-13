76be15e56b889ec0039fab5109b655be425dea3f
ivankisakye <148990697+ivankisakye@users.noreply.github.com>
2026-05-13 14:13:32 +0300

    name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -90,7 +90,7 @@ const projects = [
 ];
 
 const teamMembers = [
-  { name: 'John Muwanga', role: 'CEO & Founder', image: '/images/prof_lady.jpg', social: { linkedin: '#', twitter: '#' } },
+  { name: 'John Muwangaazi', role: 'CEO & Founder', image: '/images/prof_lady.jpg', social: { linkedin: '#', twitter: '#' } },
   { name: 'Sarah Namutebi', role: 'CTO', image: '/images/prof_lady.jpg', social: { linkedin: '#', twitter: '#' } },
   { name: 'Michael Okello', role: 'Lead Developer', image: '/images/prof_lady.jpg', social: { linkedin: '#', twitter: '#' } },
   { name: 'Grace Nambi', role: 'Project Manager', image: '/images/prof_lady.jpg', social: { linkedin: '#', twitter: '#' } },
