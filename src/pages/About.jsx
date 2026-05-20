325197f5732ca225ddb29367d51ca39f50312af0
ivankisakye <148990697+ivankisakye@users.noreply.github.com>
2026-05-20 12:28:18 +0300

    gab3

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -31,7 +31,7 @@ export default function About() {
   { name: 'Andinda Moses', role: 'CEO & Founder', image: '/images/gt2pass.jpg', social: { linkedin: '#', twitter: '#' } },
   { name: 'Ella Namutebi', role: 'CTO', image: '/images/prof_lady.jpg', social: { linkedin: '#', twitter: '#' } },
   { name: 'Mohammad Zulqif', role: 'Lead Developer', image: '/images/moh.jpg', social: { linkedin: '#', twitter: '#' } },
-  { name: 'Gabriel Kisakye', role: 'Project Manager', image: '/images/gab.jpg', social: { linkedin: '#', twitter: '#' } },
+  { name: 'Gabriel Kisakye', role: 'Project Manager', image: '/images/gab3.jpg', social: { linkedin: '#', twitter: '#' } },
 ];
 
   const milestones = [
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -93,7 +93,7 @@ const teamMembers = [
   { name: 'Andinda Moses', role: 'CEO & Founder', image: '/images/gt2pass.jpg', social: { linkedin: '#', twitter: '#' } },
   { name: 'Ella Namutebi', role: 'CTO', image: '/images/prof_lady.jpg', social: { linkedin: '#', twitter: '#' } },
   { name: 'Mohammad Zulqif', role: 'Lead Developer', image: '/images/moh.jpg', social: { linkedin: '#', twitter: '#' } },
-  { name: 'Gabriel Kisakye', role: 'Project Manager', image: '/images/gab.jpg', social: { linkedin: '#', twitter: '#' } },
+  { name: 'Gabriel Kisakye', role: 'Project Manager', image: '/images/gab3.jpg', social: { linkedin: '#', twitter: '#' } },
 ];
 
 const testimonials = [
