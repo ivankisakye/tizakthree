6fa69d45616a9e1651935b36bacaf5867d0aed0d
ivankisakye <148990697+ivankisakye@users.noreply.github.com>
2026-05-26 17:42:09 +0300

    effective

--- src/pages/Policies.jsx
+++ src/pages/Policies.jsx
@@ -296,13 +296,9 @@ export default function Policies() {
             We believe in full transparency. Read our Privacy Policy, Terms &amp; Conditions, and Community Guidelines that govern your use of Pangisa.
           </p>
 
-          {/* Effective date badge */}
-          <div className="mt-6 inline-flex items-center gap-2 bg-white/10 rounded-lg px-4 py-2">
-            <svg className="w-4 h-4 text-green-400" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
-            </svg>
-            <span className="text-white/80 text-xs sm:text-sm">Effective Date: May 2026</span>
-          </div>
+          
+
+
         </div>
       </section>
 
